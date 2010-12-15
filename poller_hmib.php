68f998cad1564294b2e952d0160f69dc1c56bfcb
Reinhard Scheck <gandalf@cacti.net>
2010-12-15 20:54:11 +0000

    providing cacti_escapeshell* functions in environments, where they don't exist yet
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/hmib/trunk@1388 67214365-cc29-0410-bfbe-843132f0f3d7

--- poller_hmib.php
+++ poller_hmib.php
@@ -27,6 +27,9 @@ chdir(dirname(__FILE__));
 chdir("../..");
 include("./include/global.php");
 include_once("./lib/poller.php");
+if (!function_exists('cacti_escapeshellcmd')) {
+    include_once("./plugins/hmib/snmp_functions.php");
+}
 include_once("./plugins/hmib/snmp.php");
 include_once("./lib/ping.php");
 ini_set("memory_limit", "128M");
