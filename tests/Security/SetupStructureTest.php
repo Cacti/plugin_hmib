074e112f5704e87068eb099734ed24e439b31033
Thomas Vincent <thomasvincent@gmail.com>
2026-08-22 03:25:09 -0700

    docs: use complete test file headers
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- tests/Security/SetupStructureTest.php
+++ tests/Security/SetupStructureTest.php
@@ -8,9 +8,19 @@
  | as published by the Free Software Foundation; either version 2          |
  | of the License, or (at your option) any later version.                  |
  |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
  | Cacti: The Complete RRDtool-based Graphing Solution                     |
  +-------------------------------------------------------------------------+
- */
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
 
 $source = plugin_test_read_source('setup.php');
 $infoFile = parse_ini_file(__DIR__ . '/../../INFO', true);
